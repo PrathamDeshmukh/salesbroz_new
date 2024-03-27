bb24a1195a081441ed96a2cec2f4b574aac4f810
PrathamDeshmukh <prathamdeshmukh86@gmail.com>
2024-03-27 13:26:10 +0530

    upload to git

---
+++ application/views/front/template.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include ('head.php');?>
+
+
+<body>
+
+    <!-- Loading Start-->
+    <!-- <div id="pq-loading">
+        <div id="pq-loading-center">
+            <img src="images/logos/logo-1.png" alt="loading">
+        </div>
+    </div> -->
+    <!--Loading End -->
+
+ <!--?php include('header.php');?>-->
+
+
+ <?php include ($page_name.'.php');?>
+
+
+
+   <?php include('footer.php');?>
+
+    <!--Back to top Start-->
+    <div id="back-to-top">
+        <a class="top" id="#top" href="#"> <i class="ion-ios-arrow-up"></i> </a>
+    </div>
+    <!--Back to top End-->
+
+   <?php include("script.php");?>
+</body>
+
+
+
+
+</html>
