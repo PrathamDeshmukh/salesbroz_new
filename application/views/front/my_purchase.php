c85dde125f8c70af37ab101c0d6d94e91400de86
vasu181 <choudharyvasu3@gmail.com>
2024-04-04 16:54:08 +0530

    change by vasu

--- application/views/front/my_purchase.php
+++ application/views/front/my_purchase.php
@@ -5,116 +5,83 @@
             <div class="col-12">
               <div class="card">
                 <div class="card-body">
-                    <input name="tags" id="tags" value="Arvind" style="height: 35px; width: 500px; margin-top: 3px;"/>
+                <input name="tags" id="tags" value="Arvind" style="height: 35px; width: 500px; margin-top: 3px;"/>
                   <div class="popover-static-demo"> 
                     <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-info">
                       <div class="arrow"></div>
                       <h3 class="popover-header">Mobiles</h3>
                       <div class="popover-body">
-                        <p>Total Count</p><br>
-                        <p>Total Value</p>
+                        <p>Total Count  :  11</p><br>
+                        <p>Total Value  :  456</p>
                       </div>
                     </div>
                     <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-warning">
                       <div class="arrow"></div>
                       <h3 class="popover-header">Accessories</h3>
                       <div class="popover-body">
-                      <p>Total Count</p><br>
-                        <p>Total Value</p>
+                      <p>Total Count  :  23</p><br>
+                        <p>Total Value  :  654</p>
                       </div>
                     </div>
                     <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-primary">
                       <div class="arrow"></div>
                       <h3 class="popover-header">Overall</h3>
                       <div class="popover-body">
-                      <p>Total Count</p><br>
-                        <p>Total Value</p>
+                        <p>Total Count  :  19</p><br>
+                        <p>Total Value  :  755</p>
                       </div>
                     </div>
-                    
                     <div class="clearfix"></div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-md-6 grid-margin stretch-card">
+          </div>
+          <div class="row" style="margin-top: 30px;">
+            
+            <div class="col-12">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">Basic tooltip</h4>
-                  <p class="card-description">Basic tooltip demo that appears on hover</p>
-                  <p>Hover the below button for interactive demo</p>
-                  <button class="btn btn-primary" data-toggle="tooltip" data-placement="right"
-                    title="Basic tooltip">Hover me</button>
-                </div>
-                <div class="card-body">
-                  <h4 class="card-title">Tooltip positions</h4>
-                  <p class="card-description">Add attribute <code>data-placement={position}</code> to the element</p>
-                  <div class="tooltip-static-demo">
-                    <div class="tooltip bs-tooltip-top bs-tooltip-top-demo" data-bs-toggle="tooltip"
-                      data-bs-placement="top" title="Tooltip on top">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner">
-                        Top Tooltip
-                      </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-right bs-tooltip-right-demo" data-bs-toggle="tooltip"
-                      data-bs-placement="right" title="Tooltip on right">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner">
-                        Right Tooltip
-                      </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo" data-bs-toggle="tooltip"
-                      data-bs-placement="bottom" title="Tooltip on bottom">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner">
-                        Bottom Tooltip
-                      </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-left bs-tooltip-left-demo" data-bs-toggle="tooltip"
-                      data-bs-placement="left" title="Tooltip on left">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner">
-                        Left Tooltip
-                      </div>
-                    </div>
+                  <div class=" md-2 d-flex justify-content-between" >
+                <input name="tags" id="tags" placeholder="Filter  by Brands" value="" style="height: 35px; width: 500px; margin-top: 3px;"/>
+                <nav>
+                    <ul class="pagination flex-wrap">
+                      <li class="page-item active"><a class="page-link" href="#">2024</a></li>
+                      <li class="page-item"><a class="page-link" href="#">March</a></li>
+                      <li class="page-item"><a class="page-link" href="#">This Week</a></li>
+                      <li class="page-item"><a class="page-link" href="#">Today</a></li>
+                    </ul>
+                  </nav>
+                  </div><br>
+                  
+                  <div class="d-flex justify-content-between">  
+                  <h6>Showing Purchases Data till <b>Mar 1,2024 - Mar 31,2024</b>(ARVIND)</h6>               
+                  <button type="button" class="btn btn-primary float-end">Export to Excel</button>
+                  </div><br>
+                
+                <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Brand</th>
+                          <th>Total Value</th>
+                          <th>Total Count</th>
+                          <th>See More</th>
+                        </tr>
+                      </thead>                      
+                    </table>
+                  </div>
+                  <div class="popover-static-demo"> 
+                    <h3><center>No Data Found</center></h3>     
+                    <div class="clearfix"></div>
                   </div>
                 </div>
-                <div class="card-body">
-                  <h4 class="card-title">Tooltip colors</h4>
-                  <p class="card-description">Add attribute <code>data-custom-class="tooltip-{color}"</code> to the
-                    element</p>
-                  <div class="tooltip-static-demo">
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo tooltip-success"
-                      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Success">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner"> Success </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo tooltip-info" data-bs-toggle="tooltip"
-                      data-bs-placement="bottom" title="Info">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner"> Info </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo tooltip-warning"
-                      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Warning">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner"> Warning </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo tooltip-primary"
-                      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Primary">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner"> Primary </div>
-                    </div>
-                    <div class="tooltip bs-tooltip-bottom bs-tooltip-bottom-demo tooltip-danger"
-                      data-bs-toggle="tooltip" data-bs-placement="bottom" title="Danger">
-                      <div class="arrow"></div>
-                      <div class="tooltip-inner"> Danger </div>
-                    </div>
-                  </div>
-                </div>
+
               </div>
             </div>
-            
           </div>
-          
         </div>
+</div>
+</div>
+        
+            
