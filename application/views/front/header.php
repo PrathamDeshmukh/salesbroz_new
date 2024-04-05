2bd1ee687fdbb14e0b367530572cd7cbaefb2a1c
PrathamDeshmukh <prathamdeshmukh86@gmail.com>
2024-04-05 15:03:03 +0530

    changes in home

--- application/views/front/header.php
+++ application/views/front/header.php
@@ -1,10 +1,10 @@
  <!-- partial:partials/_navbar.html -->
  <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-    <a class="navbar-brand brand-logo me-5" href=""><img src="/assets/images/logo.svg" class="me-2"
-        alt="logo" /></a>
-    <a class="navbar-brand brand-logo-mini" href=""><img src="/assets/images/logo-mini.svg"
-        alt="logo" /></a>
+    <a class="navbar-brand brand-logo me-5" href=""><img src="/assets/images/SalesBroz.png" class="me-2"
+        alt="SalesBroz" /></a>
+    <a class="navbar-brand brand-logo-mini" href=""><img src="/assets/images/SalesBroz.png"
+        alt="logoo" /></a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
     <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
