f103ddefc88e401eb3a06bc82b08f25aa46e4a13
PrathamDeshmukh <prathamdeshmukh86@gmail.com>
2024-04-15 17:29:55 +0530

    footer in center

--- application/views/front/footer.php
+++ application/views/front/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+          <div class="d-sm-flex justify-content-center">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024. All Right Reserved By SalesBroz <a href="https://www.bootstrapdash.com/" target="_blank"></a></span>
             <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><i class="ti-heart text-danger ms-1"></i></span>
           </div>
