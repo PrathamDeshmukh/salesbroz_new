69f2acf827817422df9ed6f8d754cb1e98e46272
vasu181 <choudharyvasu3@gmail.com>
2024-04-18 14:25:27 +0530

    change in my stock

---
+++ application/views/front/add_your_model.php
@@ -0,0 +1,106 @@
+
+
+
+
+
+    <!--main panel-->
+    <div class="main-panel">
+        <div class="content-wrapper">
+        <div class="row">
+          <div class="col-md-12 grid-margin transparent">
+              <div class="row">
+                <div class="col-md-5 mb-4  transparent">
+                  <h2 class="font-weight-bold">My Products</h2>
+                </div>
+                
+                  <!-- <input  type="text" class="form-control" name="tags" id="tags" value="Arvind" />
+                </div>
+                <div class="col-md-2 mb-1  justify-content-end transparent">
+                  <button type="button" class="btn btn-primary float-end">Filter</button>
+                </div>
+                <div class="col-md-2 mb-1 justify-content-end transparent">
+                  <button type="button" class="btn btn-primary float-end">Download</button> -->
+              </div>
+            </div>
+            
+          </div>
+          <div class="row">
+           
+            <div class="col-md-12 grid-margin transparent">
+              <div class="card ">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-md-6 mb-4 transparent">
+                      <input type="text" class="form-control p-2" placeholder="Search for products, brands and more" value="" >
+                    </div>
+                    <div class="col-md-6 mb-4 transparent">
+                      <a class="btn btn-primary float-end" href="<?php echo base_url();?>">+ New Products</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table table-striped table-borderless">
+                        
+                    
+                      <thead>
+                        <tr>
+                          <th>Brand</th>
+                          <th>Model</th>
+                          <th>Colours</th>
+                          <th>Dp₹</th>
+                          <th>Mop₹</th>
+                          <th>Mrp₹</th>
+                          <th>setting</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Samsung</td>
+                          <td>S24 ultra</td>
+                          <td>Black</td>
+                          <td>102000₹</td>
+                          <td>1022000₹</td>
+                          <td>1025000₹</td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>Apple</td>
+                          <td>iphone 14pro</td>
+                          <td>Golden</td>
+                          <td>103000₹</td>
+                          <td>1032000₹</td>
+                          <td>1035000₹</td>
+                          <td>-</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    
+                    <nav>
+                    <ul class="pagination flex-wrap">
+                      <li class="page-item"><a class="page-link" href="#"><i class="ti-angle-left"></i></a></li>
+                      <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                      <li class="page-item"><a class="page-link" href="#">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item"><a class="page-link" href="#">4</a></li>
+                      <li class="page-item"><a class="page-link" href="#"><i class="ti-angle-right"></i></a></li>
+                    </ul>
+                  </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+</div>
+</div>
+</div>
+</div>
+
---
+++ application/views/front/my_stock.php
@@ -0,0 +1,143 @@
+<div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin transparent">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-md-6 mb-4 stretch-card transparent">
+                    <input  type="text" class="form-control p-2" name="tags" id="tags" placeholder="Filter Here" value="" />
+                    </div>
+                    <div class="col-md-6 mb-4 transparent">
+                      <a class="btn btn-primary float-end" href="">+ Add Stocks</a>
+                    </div>
+                    
+                  </div>
+                  <div class="row">
+                  <div class="col-12">
+                    <div class="popover-static-demo">                   
+                    <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-info">
+                      <div class="arrow"></div>
+                      <h3 class="popover-header">Mobiles</h3>
+                      <div class="popover-body">
+                        <p>Total Count  :  4</p><br>
+                        <p>Total Value  :  49,978</p>
+                      </div>
+                    </div>
+                    <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-warning">
+                      <div class="arrow"></div>
+                      <h3 class="popover-header">Accessories</h3>
+                      <div class="popover-body">
+                      <p>Total Count  :  -</p><br>
+                      <p>Total Value  :  -</p>
+                      </div>
+                    </div>
+                    <div class="popover bs-popover-bottom bs-popover-bottom-demo popover-primary">
+                      <div class="arrow"></div>
+                      <h3 class="popover-header">Overall</h3>
+                      <div class="popover-body">
+                      <p>Total Count  :  4</p><br>
+                      <p>Total Value  :  49,978</p>
+                      </div>
+                    </div> 
+                    <div class="clearfix"></div>
+                    </div>
+                  </div>
+                  </div>
+                    <div class="row">
+                      <div class="col-md-6 stretch-card transparent">
+                    Showing Stocks Data for
+                      </div>
+                    </div>
+                    <div class="row">
+                    <div class="col-md-6 mb-2 transparent float-end">
+                    <input type="text" class="form-control p-2" placeholder="Search Stock" value="" >
+                    </div>
+                    <div class="col-md-6 mb-2 transparent">
+                      <a class="btn btn-primary float-end" href="">Export to Excel</a>
+                    </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                
+                  <div class="table-responsive">
+                    <table class="table table-striped table-borderless">
+                    
+                      <thead>
+                        <th>XIAOMI 27,998(2 units)</th>
+                        <tr>
+                          <th>Model</th>
+                          <th>Quantity</th>
+                          <th>Total Value</th>
+                          <th>Settings</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Poco M2 Pro 4/64GB</td>
+                          <td>2</td>
+                          <td>27,998</td>
+                          <td>Show IMEIs</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                
+                  <div class="table-responsive">
+                    <table class="table table-striped table-borderless">
+                    
+                      <thead>
+                        <th>VIVO 21,980(2 units)</th>
+                        <tr>
+                          <th>Model</th>
+                          <th>Quantity</th>
+                          <th>Total Value</th>
+                          <th>Settings</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Y20 4/64GB</td>
+                          <td>1</td>
+                          <td>12,990</td>
+                          <td>Show IMEIs</td>
+                        </tr><tr>
+                          <td>Y11 3/32GB</td>
+                          <td>1</td>
+                          <td>8,990</td>
+                          <td>Show IMEIs</td>
+                        </tr>
+
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+</div>
+</div>
+</div>
+          </div> 
+        </div>
+</div>
+</div>
+        
+            
